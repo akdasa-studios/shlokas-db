11025346d5ace8ce3c2a465e63b122c1718cfc89
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-04-14 15:14:41 +0200

    feat: configurable content manager account

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 npm install
+
+echo "Preparing configs..."
+find . -type f | xargs sed -i "s/{{CONTENT_MANAGER_LOGIN}}/${CONTENT_MANAGER_LOGIN}/g"
+find . -type f | xargs sed -i "s/{{CONTENT_MANAGER_PASSWORD}}/${CONTENT_MANAGER_PASSWORD}/g"
+
 echo "Bootstrapping database ${BOOTSTRAP_DB_HOST}..."
 npx couchdb-bootstrap ${BOOTSTRAP_DB_HOST} config
